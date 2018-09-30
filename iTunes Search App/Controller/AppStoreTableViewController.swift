b32588a9c3e9feb810384725a109bfb63a1b8654
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-09-30 23:14:00 +0530

    Loading data in to the table view

---
+++ iTunes Search App/Controller/AppStoreTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  AppStoreTableViewController.swift
+//  iTunes Search App
+//
+//  Created by Dhanuka Perera on 9/30/18.
+//  Copyright © 2018 Dhanuka Perera. All rights reserved.
+//
+
+import UIKit
+
+class AppStoreTableViewController: UITableViewController {
+
+    var apps: [App]?
+    var appStoreClient = AppStoreClient()
+    
+    struct Storyboard {
+        static let appCell = "AppCell"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+          fetchApps()
+        
+    }
+    
+    func fetchApps()
+    {
+        appStoreClient.fetchApps(withTerm: "Facebook", inEntity: "software") { (apps) in
+            self.apps = apps
+            print(self.apps)
+            self.tableView.reloadData()
+        }
+    }
+    
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let apps = apps {
+            return apps.count
+        }
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.appCell, for: indexPath) as! AppTableViewCell
+        
+        cell.app = apps?[indexPath.row]
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+}
