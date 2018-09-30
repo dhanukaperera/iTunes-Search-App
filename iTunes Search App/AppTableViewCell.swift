86155e06912ffb6fb7726704b445b5490fd18ee6
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-09-30 22:27:39 +0530

    update the UI labels

--- iTunes Search App/AppTableViewCell.swift
+++ iTunes Search App/AppTableViewCell.swift
@@ -15,4 +15,20 @@ class AppTableViewCell: UITableViewCell {
     @IBOutlet weak var appDescriptionLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     
+    var app: App! {
+        didSet {
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        appNameLabel.text = app.name
+        appDescriptionLabel.text = app.description
+        
+        if app.price == 0 {
+            priceLabel.text = app.formattedPrice
+        } else {
+            priceLabel.text = "$\(app.price)"
+        }
+    }
 }
