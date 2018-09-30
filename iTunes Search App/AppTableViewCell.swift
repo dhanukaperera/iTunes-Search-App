30cd39196f6e1b32532583151e47d0078df92be5
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-09-30 22:24:17 +0530

    connect the UI outlets with the tableCell Controller

---
+++ iTunes Search App/AppTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  AppTableViewCell.swift
+//  iTunes Search App
+//
+//  Created by Dhanuka Perera on 9/30/18.
+//  Copyright © 2018 Dhanuka Perera. All rights reserved.
+//
+
+import UIKit
+
+class AppTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var appNameLabel: UILabel!
+    @IBOutlet weak var appDescriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+}
