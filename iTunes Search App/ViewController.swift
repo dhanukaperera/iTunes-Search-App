05a51cb3e55055d7ae6bcc01ecf9c3598cda3728
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-09-30 21:03:23 +0530

    Initial Commit

---
+++ iTunes Search App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunes Search App
+//
+//  Created by Dhanuka Perera on 9/30/18.
+//  Copyright © 2018 Dhanuka Perera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
