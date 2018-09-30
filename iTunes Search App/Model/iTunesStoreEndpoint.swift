1abdd14504bc2f208314c6eb7a9a19b9431679fc
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-09-30 21:16:56 +0530

    Building the full endpoint URL with the parameters.

--- iTunes Search App/Model/iTunesStoreEndpoint.swift
+++ iTunes Search App/Model/iTunesStoreEndpoint.swift
@@ -14,25 +14,64 @@ enum iTunesStoreEndpoint {
     
     case search(term: String, entity: String)
     
+    // Setting up the base URL of iTunes endpoint
     private var baseURL: String {
         return "https://itunes.apple.com/"
     }
     
+    // adding the search? path to endpoint
     private var path: String {
         switch self {
         case .search: return "/search"
         }
     }
     
+    // Defining the paremeters of the endpoint
     private struct ParameterKeys {
         static let country = "country"
         static let term = "term"
         static let entity = "entity"
     }
     
+    // Setting defult values for the endpoint parameters.
     private struct DefaultValues {
         static let country = "us"
-        static let term = "apple"
+        static let term = "Puzzle"
+    }
+    
+    private var parameters: [String : Any] {
+        switch self {
+        case .search(let term, let entity):
+            let parameters: [String : Any] = [
+                ParameterKeys.term : term,
+                ParameterKeys.country : DefaultValues.country,
+                ParameterKeys.entity : entity
+            ]
+            
+            return parameters
+        }
+    }
+    
+    // adding parametermeters to the qeury component.
+    private var queryComponents: [URLQueryItem] {
+        var components = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.append(queryItem)
+        }
+        
+        return components
+    }
+    
+    // Buliding the full endpoint URL
+    var request: URLRequest {
+        var components = URLComponents(string: baseURL)!
+        components.path = path
+        components.queryItems = queryComponents
+        
+        let url = components.url!
+        return URLRequest(url: url)
     }
     
     
